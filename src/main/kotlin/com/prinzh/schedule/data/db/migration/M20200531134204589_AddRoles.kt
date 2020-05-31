f1081059f1ca7b9af812fe9841e668bf64455043
ShkiTTer <shkipper65@gmail.com>
2020-05-31 13:54:13 +0700

    Add db migration
    Add migration for create role and user_role tables

---
+++ src/main/kotlin/com/prinzh/schedule/data/db/migration/M20200531134204589_AddRoles.kt
@@ -0,0 +1,22 @@
+package com.prinzh.schedule.data.db.migration
+
+import com.improve_future.harmonica.core.AbstractMigration
+import com.prinzh.schedule.data.db.entity.Roles
+import com.prinzh.schedule.data.db.entity.UserRoles
+import com.prinzh.schedule.data.db.entity.Users
+import org.jetbrains.exposed.sql.SchemaUtils
+
+/**
+ * Migration
+ */
+class M20200531134204589_AddRoles : AbstractMigration() {
+    override fun up() {
+        removeColumn(Users.tableName, "role")
+        SchemaUtils.create(Roles, UserRoles)
+    }
+
+    override fun down() {
+        SchemaUtils.drop(UserRoles, Roles)
+        addIntegerColumn(UserRoles.tableName, "role")
+    }
+}
