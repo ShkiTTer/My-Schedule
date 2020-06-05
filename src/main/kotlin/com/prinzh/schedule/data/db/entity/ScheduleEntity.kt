2acdca556dde1a4d16cb90aa9ed1c7a8855afb8a
ShkiTTer <shkipper65@gmail.com>
2020-06-05 22:54:06 +0700

    Add new reference in Schedule entity

--- src/main/kotlin/com/prinzh/schedule/data/db/entity/ScheduleEntity.kt
+++ src/main/kotlin/com/prinzh/schedule/data/db/entity/ScheduleEntity.kt
@@ -11,6 +11,7 @@ object Schedules : UUIDTable("schedule") {
     val group = reference("group", Groups, ReferenceOption.CASCADE)
     val discipline = reference("discipline", TeacherDisciplines, ReferenceOption.CASCADE)
     val audience = reference("audience", Audiences, ReferenceOption.CASCADE)
+    val type = reference("type", LessonTypes, ReferenceOption.RESTRICT)
 
     val day = integer("day").check { column ->
         column.between(1, 7)
@@ -26,6 +27,7 @@ class ScheduleEntity(id: EntityID<UUID>): UUIDEntity(id) {
     var group by GroupEntity referencedOn Schedules.group
     var discipline by TeacherDisciplineEntity referencedOn Schedules.discipline
     var audience by AudienceEntity referencedOn Schedules.audience
+    var type by LessonTypeEntity referencedOn Schedules.type
 
     var day by Schedules.day
     var time by Schedules.time
