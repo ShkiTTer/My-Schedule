7063967e3b286be6a8bb9392dd038d4132b033af
ShkiTTer <shkipper65@gmail.com>
2020-06-17 19:45:05 +0700

    Add role service to DI

--- src/main/kotlin/com/prinzh/schedule/app/di/KoinModules.kt
+++ src/main/kotlin/com/prinzh/schedule/app/di/KoinModules.kt
@@ -69,6 +69,12 @@ val serviceModule = module {
             refreshTokenRepository = get()
         )
     }
+
+    single<IRoleService> {
+        RoleServiceImpl(
+            roleRepository = get()
+        )
+    }
 }
 
 @KtorExperimentalAPI
--- src/main/kotlin/com/prinzh/schedule/app/services/RoleServiceImpl.kt
+++ src/main/kotlin/com/prinzh/schedule/app/services/RoleServiceImpl.kt
@@ -1,9 +1,11 @@
-package com.prinzh.schedule.app.services.interfaces
+package com.prinzh.schedule.app.services
 
 import com.prinzh.schedule.app.responses.RoleResponse
+import com.prinzh.schedule.app.services.interfaces.IRoleService
 import com.prinzh.schedule.domain.repository.IRoleRepository
 
-class RoleServiceImpl(private val roleRepository: IRoleRepository): IRoleService {
+class RoleServiceImpl(private val roleRepository: IRoleRepository):
+    IRoleService {
     override suspend fun getAll(): List<RoleResponse> {
         return roleRepository.getAll().map {
             RoleResponse.fromDomain(it)
